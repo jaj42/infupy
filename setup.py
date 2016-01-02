2bf953ea21963ef5cad99e69477535fb1245a43d
Jona Joachim <jaj@joachim.cc>
2016-01-02 12:51:45 +0100

    Turn this into an actual python package. Add files github expects.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name         = 'infupy',
+      version      = '0.1',
+      description  = 'Syringe pump infusion',
+      url          = 'https://github.com/jaj42/infupy',
+      author       = 'Jona Joachim',
+      author_email = 'jona@joachim.cc',
+      license      = 'ISC',
+      packages     = ['funniest'],
+      zip_safe     = False)
