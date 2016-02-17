f611811bb53628937597cfb032e659de02c34fcf
jaj <jona@joachim.cc>
2016-02-17 21:34:29 +0100

    GUI async WIP. starts looking good.

--- scripts/syringoreceuil.py
+++ scripts/syringoreceuil.py
@@ -3,41 +3,96 @@
 from PyQt4 import QtCore, QtGui
 
 import infupy.backends.fresenius as fresenius
-import infupy.gui.syringorecueil as syringorecueil 
 from infupy.gui.syringorecueil_ui import Ui_wndMain
 
 import sys, time, csv
 
 class DeviceWorker(QtCore.QObject):
-    sigConnected      = QtCore.pyqtSignal() # XXX should open new file
-    sigDisconnected   = QtCore.pyqtSignal() # XXX should close file
+    sigConnected      = QtCore.pyqtSignal()
+    sigDisconnected   = QtCore.pyqtSignal()
     sigUpdateSyringes = QtCore.pyqtSignal(list)
 
     def __init__(self):
+        super(DeviceWorker, self).__init__()
+        self.port = ""
         self.conn = None
         self.base = None
+        self.csvfd = None
+        self.csv = None
         self.syringes = []
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.loop)
 
+    def setport(self, port):
+        self.port = port
+
     def start(self):
-        self.timer.start(5000)
+        self.timer.start(5000) # 5 seconds
 
     def stop(self):
         self.timer.stop()
 
-    def loop(self, device):
-        print("loop")
-        return
+    def loop(self):
+        if self.conn is None:
+            try:
+                print("Trying to connect to " + self.port)
+                self.conn = fresenius.FreseniusComm(self.port)
+            except:
+                self.conn = None
+                self.onDisconnected()
+                return
+
         if self.base is None:
             try:
                 self.base = fresenius.FreseniusBase(self.conn)
             except:
-                self.sigDisconnected.emit()
-                return
+                self.onDisconnected()
             else:
-                self.sigConnected.emit()
+                self.onConnected()
+
+    def onConnected(self):
+        self.sigConnected.emit()
+        self.newFile()
+        self.connectSyringes()
+
+    def onDisconnected(self):
+        self.sigDisconnected.emit()
+        if self.csvfd is not None:
+            self.csvfd.close()
+
+    def connectSyringes(self):
+        modids = self.base.listModules()
+        self.sigUpdateSyringes.emit(modids)
+        for modid in modids:
+            s = fresenius.FreseniusSyringe(self.conn, modid)
+            s.addCallback(self.cbLogValues)
+            s.registerEvent(fresenius.VarId.volume)
+            self.syringes.append(s)
+
+    def newFile(self):
+        if self.csvfd is not None:
+            self.csvfd.close()
+        filename = time.strftime('%Y%m%d-%H%M.csv')
+        self.csvfd = open(filename, 'w', newline='')
+        self.csv = csv.DictWriter(self.file, fieldnames = ['time', 'syringe', 'volume'])
+        self.csv.writeheader()
+
+    def cbLogValues(origin, msg):
+        try:
+            volume = fresenius.extractVolume(msg)
+        except ValueError:
+            return
+
+        if origin is not None and origin.isdigit():
+            syringe = int(origin)
+        else:
+            return
+
+        print("{}:{}".format(syringe, volume))
+        self.csv.writerow({'time'    : time.time(),
+                           'syringe' : syringe,
+                           'volume'  : volume})
 
 
 class MainUi(QtGui.QMainWindow, Ui_wndMain):
@@ -45,93 +100,40 @@ class MainUi(QtGui.QMainWindow, Ui_wndMain):
         super(MainUi, self).__init__(parent = parent)
         self.setupUi(self)
 
-        self.conn = None
-        self.base = None
-        self.syringes = []
-
-        self.statusBar.showMessage('Déconnecté')
-
-        # Connect callbacks
-        self.btnConnect.clicked.connect(self.connect)
-        self.btnDisconnect.clicked.connect(self.disconnect)
-        self.btnStart.clicked.connect(self.start)
-        self.btnStop.clicked.connect(self.stop)
-        self.btnSUpdate.clicked.connect(self.updatelist)
-
+        # Init worker
         self.__workerthread = QtCore.QThread()
         self.__worker = DeviceWorker()
-        self.__rpc.moveToThread(self.__workerthread)
+
+        # Worker callbacks and signals
+        self.comboCom.editTextChanged.connect(self.__worker.setport)
+        self.__worker.setport(self.comboCom.currentText())
+
+        self.__worker.sigConnected.connect(self.connected)
+        self.__worker.sigDisconnected.connect(self.disconnected)
+        self.__worker.sigUpdateSyringes.connect(self.updateSyringeList)
+
+        self.__worker.moveToThread(self.__workerthread)
         self.__worker.start()
 
-    def connect(self):
-        port = self.comboCom.currentText()
-        try:
-            self.conn = fresenius.FreseniusComm(port)
-            self.base = fresenius.FreseniusBase(self.conn)
-        except:
-            self.conn = None
-            self.statusBar.setStyleSheet("QStatusBar{background : red;}")
-            self.statusBar.showMessage('Erreur de connexion')
-        else:
-            self.statusBar.setStyleSheet("QStatusBar{background : green;}")
-            self.statusBar.showMessage('Connexion ok')
-            self.filename = time.strftime('%Y%m%d-%H%M.csv')
-            self.file = open(self.filename, 'w', newline='')
-            self.csv = csv.DictWriter(self.file, fieldnames = ['time', 'syringe', 'volume'])
-            self.csv.writeheader()
-            self.updatelist()
+    def connected(self):
+        self.statusBar.setStyleSheet("QStatusBar{background : green;}")
+        self.statusBar.showMessage('Connexion ok')
 
-    def disconnect(self):
-        self.syringes = []
-        self.base = None
-        time.sleep(1)
-        self.file.close()
-        self.conn.close()
-        self.conn = None
-        self.statusBar.setStyleSheet("QStatusBar{background : None;}") 
-        self.statusBar.showMessage('Déconnecté')
-
-    def updatelist(self):
-        def logValues(origin, msg):
-            try:
-                volume = fresenius.extractVolume(msg)
-            except ValueError:
-                return
-
-            if origin is not None and origin.isdigit():
-                syringe = int(origin)
-            else:
-                return
-
-            print("{}:{}".format(syringe, volume))
-            self.csv.writerow({'time'    : time.time(),
-                               'syringe' : syringe,
-                               'volume'  : volume})
-
-        self.syringes = []
+    def disconnected(self):
         self.lstSyringes.clear()
-        if self.base is None: return
-        modids = self.base.listModules()
-        for modid in modids:
-            s = fresenius.FreseniusSyringe(self.conn, modid)
-            s.addCallback(logValues)
-            self.syringes.append(s)
-            #drugname = s.readDrug()
+        self.statusBar.setStyleSheet("QStatusBar{background : red;}")
+        self.statusBar.showMessage('Deconnecte')
+
+    def updateSyringeList(slist):
+        self.lstSyringes.clear()
+        for modid in slist:
             liststr = "Seringue {}".format(modid)
             self.lstSyringes.addItem(liststr)
-
-    def start(self):
-        for s in self.syringes:
-            s.registerEvent(fresenius.VarId.volume)
-
-    def stop(self):
-        for s in self.syringes:
-            s.clearEvents()
 
 
 qApp = QtGui.QApplication(sys.argv)
 
-wMain = syringorecueil.MainUi()
+wMain = MainUi()
 wMain.show()
 
 sys.exit(qApp.exec_())
